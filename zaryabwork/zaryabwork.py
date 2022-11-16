074703d082eb18cc1b55a55a993301ab2460d2d7
Zaryabali1 <zaryabalined@gmail.com>
2022-11-16 22:10:06 +0500

    my portion of code

---
+++ zaryabwork/zaryabwork.py
@@ -0,0 +1,16 @@
+from kivy.app import App
+from kivy.uix.screenmanager import Screen, ScreenManager
+
+class HomeScreen(Screen):
+    pass
+class FirstScreen(Screen):
+    pass
+Class Manager(ScreenManager):
+    pass
+
+class MyApp(App):
+    def build(self):
+        return super().build(self)
+        pass
+
+MyApp().run()
