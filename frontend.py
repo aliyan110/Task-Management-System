6d8902438c3931d15544af47b677461b846486d4
aliyan110 <aliyanmujahid2000@gmail.com>
2022-11-18 00:09:31 +0500

    Modified frontend file for registration page

--- frontend.py
+++ frontend.py
@@ -1,25 +1,41 @@
-import kivy
-# import kivy 
-# kivy.require('1.9.1') # replace with your current kivy version !
-
-# from kivy import Config
-# Config.set('graphics', 'multisamples', '0')
-
 from kivy.app import App
-from kivy.uix.label import Label
-from kivy.uix.gridlayout import GridLayout
+from kivy.graphics import *
+from kivy.graphics import Rectangle
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.image import Image
+from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
+from kivy.core.window import Window
+from kivy.uix.anchorlayout import AnchorLayout
 
-class MyGridLayout(GridLayout):
+class MyLayout(BoxLayout):
     def __init__(self, **kwargs):
-        super(MyGridLayout, self). __init__(**kwargs)
-        
+        super(MyLayout, self). __init__(**kwargs)
         self.cols = 1
+        self.orientation= 'vertical'
 
+        #image widget self.canvas.before will set image Pic.png as background
+        with self.canvas.before:
+            self.bg= Rectangle(pos=self.pos, size=self.size, source='Pic.png')
+
+        # adding heading on the registration page
+        self.add_widget(Label(text="REGISTRATION",
+            font_size=26, 
+            pos_hint={"center_x":0.5,"center_y":0.1},
+            size_hint_x= None,
+            height=70,
+            size_hint_y= None,
+            width = 300))
         # Add Another Grid for text to be entered and one main grid for submit button and result
         self.top_grid = GridLayout()
         self.top_grid.cols = 2
+        self.top_grid.size_hint= (0.6,0.8)
+        self.top_grid.pos_hint= {"center_x":0.5, "center_y":0.5}
+
+
 
         # Add widget
         self.top_grid.add_widget(Label(text="Name"))
@@ -56,12 +72,31 @@ class MyGridLayout(GridLayout):
         # Add top_grid into main grid as a widget
         self.add_widget(self.top_grid)
 
-        # Creating Submit Button
-        self.submit = Button(text="Submit", font_size=18,
+        # Creating Image Upload Button
+        self.upload = Button(text="Upload Image", font_size=22,
+        pos_hint={"center_x":0.5,"center_y":0.6},
+        size_hint_x= None,
+        height=70,
         size_hint_y= None,
+        width = 250)
+        self.upload.bind(on_press = self.push)
+        self.add_widget(self.upload)
+        self.add_widget(Label(text="or drop a file",
+            font_size=18, 
+            pos_hint={"center_x":0.5,"center_y":0.7},
+            size_hint_x= None,
+            height=70,
+            size_hint_y= None,
+            width = 250))
+
+        # Creating Submit Button
+        self.submit = Button(text="Register", font_size=18,
+        pos_hint={"center_x":0.7},
+        size_hint_x= None,
         height=50,
-        size_hint_x = None,
+        size_hint_y= None,
         width = 200)
+
         self.submit.bind(on_press = self.press)
         self.add_widget(self.submit)
     def press(self, instance):
@@ -82,9 +117,11 @@ class MyGridLayout(GridLayout):
         self.username.text =''
         self.password.text =''
         self.cpass.text =''
+    def push(self, instance):
+        pass
 class MyApp(App):
     def build(self):
-        return MyGridLayout()
+        return MyLayout()
 
 if __name__ == '__main__':
     MyApp().run()
